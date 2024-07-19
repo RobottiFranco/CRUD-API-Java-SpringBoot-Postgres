39846accea9fb395f4d632c6919ed262086e40aa
RobottiFranco <franco.robotti0@gmail.com>
2024-07-19 13:34:21 -0300

    remove useless import

--- src/main/java/com/pasteleria/crudApp/Service/impl/ProductServiceImpl.java
+++ src/main/java/com/pasteleria/crudApp/Service/impl/ProductServiceImpl.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pasteleria.crudApp.Exception.ProductServiceException;
-import com.pasteleria.crudApp.Exception.ProductTypeServiceException;
 import com.pasteleria.crudApp.Model.Product;
-import com.pasteleria.crudApp.Model.ProductType;
 import com.pasteleria.crudApp.Repository.ProductRepository;
 import com.pasteleria.crudApp.Service.ProductService;
 
